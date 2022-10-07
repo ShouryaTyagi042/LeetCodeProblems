4f358e50fc1a1a335acc856c5c17bfaa231f8890
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-10-07 21:36:07 +0530

    Created the repo

---
+++ Solutions/src/Arrays/MoveZeroes.java
@@ -0,0 +1,10 @@
+package Arrays;
+public class MoveZeroes {
+    public static void main(String[] args) {
+
+    }
+    static void moveZeroes(int[] nums) {
+
+
+    }
+}
---
+++ Solutions/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This repo is made to keep track of the leetCode solutions");
+    }
+}
