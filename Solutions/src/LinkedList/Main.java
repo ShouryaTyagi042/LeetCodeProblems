e63373638bc0839eecb7a11a245a953be892522a
Shourya Tyagi <shouryatyagi042@gmail.com>
2022-10-13 19:34:04 +0530

    Update Main.java

--- Solutions/src/LinkedList/Main.java
+++ Solutions/src/LinkedList/Main.java
@@ -8,8 +8,12 @@ public class Main {
      ListNode() {}
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
 
 
     }
+
 
     }
