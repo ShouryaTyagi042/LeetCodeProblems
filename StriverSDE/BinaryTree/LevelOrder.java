0b306043258f287929b4ca68b80c856b6bc68ae3
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-03-17 22:57:00 +0530

    Added level Order Traversal

---
+++ StriverSDE/BinaryTree/LevelOrder.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class solution {
+    public List<List<Integer>> levelorder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>() ;
+        List<List<Integer>> wraplist = new LinkedList<List<Integer>>() ;
+        if(root == null) return wraplist ;
+        queue.offer(root) ;
+        while(queue.isEmpty()) {
+            int levelSize = queue.size() ;
+            List<Integer> sublist = LinkedList<Integer>() ;
+            for(int i = 0 ; i < levelSize ; i++ ){
+                if(queue.peek().left != null) queue.offer(queue.peek().left) ;
+                if(queue.peek().right != null) queue.offer(queue.peek().right) ;
+                sublist.add(queue.poll().val) ;
+            }
+            wraplist.add(sublist) ;
+        }
+        return wraplist ;
+    }
+}
