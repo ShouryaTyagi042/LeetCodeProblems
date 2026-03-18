c25780425e5b11a3ef6c978048ff1cfd08c0011e
ShouryaTyagi042 <shouryatyagi042@gmail.com>
2026-03-18 22:12:55 +0530

    Arrays Assignment AlgoZenith

---
+++ AlgoZenith/CohortAssignments/Arrays/Replacement/Main.java
@@ -0,0 +1,139 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    // -------- FAST INPUT --------
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c, sign = 1;
+            long val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            while (c > ' ') {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+
+    static final long MOD = 1_000_000_007L;
+
+    static long modPow(long base, long exp, long mod) {
+        long result = 1 ;
+        base %= mod ;
+        while( exp > 0) {
+            // check if power is odd
+            if  ((exp & 1) == 1 ) {
+                result = (( result % mod) * (base % mod) ) % mod ;
+            }
+            base = ( (base % mod)  * (base % mod ) ) % mod ;
+            exp >>= 1 ;
+        }
+
+        return result ;
+    }
+
+    static long inverse(long n) {
+        return modPow(n, MOD - 2, MOD) ;
+    }
+
+    static class Pair  {
+        long x, y;
+
+        Pair(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
+    }
+
+    // -------- MAIN --------
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+        StringBuilder out = new StringBuilder();
+
+        int t = fs.nextInt();   // number of test cases
+
+        while (t-- > 0) {
+            int n = fs.nextInt() ;
+            int[] arr = new int[n] ;
+            for(int i = 0 ; i < n ; i ++) {
+                arr[i] = fs.nextInt() ;
+                if(arr[i] > 0) {
+                    out.append(1).append(" ");
+                } else if (arr[i] < 0) {
+                    out.append(2).append(" ");
+                }  else {
+                    out.append(0).append(" ");
+                }
+            }
+            out.append('\n') ;
+        }
+        System.out.println(out);
+
+    }
+
+}
+
