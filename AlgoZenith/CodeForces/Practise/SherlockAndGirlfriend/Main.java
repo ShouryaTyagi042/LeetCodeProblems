8e331af865a842634485dcc05fd036f545e4f767
ShouryaTyagi042 <shouryatyagi042@gmail.com>
2026-03-30 10:00:00 +0530

    776B CodeForces Practise

---
+++ AlgoZenith/CodeForces/Practise/SherlockAndGirlfriend/Main.java
@@ -0,0 +1,168 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    // -------- FAST INPUT --------
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c, sign = 1;
+            long val = 0;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            if (c == '-') {
+                sign = -1;
+                c = readByte();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ');
+
+            while (c > ' ') {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int c;
+
+            // skip any leftover newline or spaces
+            while ((c = readByte()) != -1 && c == '\n');
+
+            // read until newline
+            while (c != -1 && c != '\n') {
+                sb.append((char) c);
+                c = readByte();
+            }
+
+            return sb.toString();
+        }
+    }
+
+    static final long MOD = 1_000_000_007L;
+
+    static long modPow(long base, long exp, long mod) {
+        long result = 1 ;
+        base %= mod ;
+        while( exp > 0) {
+            // check if power is odd
+            if  ((exp & 1) == 1 ) {
+                result = (( result % mod) * (base % mod) ) % mod ;
+            }
+            base = ( (base % mod)  * (base % mod ) ) % mod ;
+            exp >>= 1 ;
+        }
+
+        return result ;
+    }
+
+    static long inverse(long n) {
+        return modPow(n, MOD - 2, MOD) ;
+    }
+
+    static class Pair  {
+        long x, y;
+
+        Pair(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
+    }
+
+    // -------- MAIN --------
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+        StringBuilder out = new StringBuilder();
+
+        int n = fs.nextInt() ;
+
+        if(n == 2 || n == 1) {
+            out.append(1).append('\n') ;
+        } else {
+            out.append(2).append('\n') ;
+        }
+
+        for(int i = 2 ; i < n + 2 ; i ++) {
+            if(checkIsPrime(i)) {
+                out.append(1).append(" ") ;
+            } else {
+                out.append(2).append(" ") ;
+            }
+        }
+
+        System.out.println(out);
+
+    }
+
+    static boolean checkIsPrime(long x) {
+        if (x == 1) {
+            return false;
+        }
+        long temp = x ;
+        for(long i = 2 ; i * i <= x ; i++) {
+            if( x % i == 0) {
+                return false;
+            }
+        }
+        if (x == temp) return true;
+        return false ;
+    }
+
+}
+
